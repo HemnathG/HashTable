bd0a54de8db24ff6950d8b4a5df680e8281b85dc
HemnathG <hemnathdavid@gmail.com>
2022-07-01 17:28:58 +0530

    Finding frequency of multiple Words

--- src/com/bridgelabz/MapMain.java
+++ src/com/bridgelabz/MapMain.java
@@ -4,11 +4,12 @@ public class MapMain {
 
 	public static void main(String[] args) {
 
-		String sentence = "To be or not to be";
-
+		String sentence = "Paranoids are not paranoid because they are paranoid but because "
+				+ "they keep putting themselves deliberately into paranoid avoidable situations";
+		
 		Map<String, Integer> map = new Map<>();
 		String[] words = sentence.toLowerCase().split(" ");
-
+		
 		for (String elements : words) {
 			System.out.println(elements);
 		}
@@ -19,10 +20,16 @@ public class MapMain {
 				value = 1;
 			else
 				value = value + 1;
-			map.add(word, value);
+			map.add(word, value);		
 		}
+		
 		map.display();
+		System.out.println();
 
+		for (String word : words) {
+			int frequency = map.get(word);
+			System.out.println("Frequency of " + word + " : " + frequency);
+		}		
 	}
 
 }
